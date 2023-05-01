4929167e627e47b2b404608c636ed2a8e96b1c43
justSmK <28645140+justSmK@users.noreply.github.com>
2023-05-01 18:31:28 +0300

    Added a square to the screen

--- Gradient and Shadow/ViewController.swift
+++ Gradient and Shadow/ViewController.swift
@@ -9,12 +9,41 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    private lazy var squareView: UIView = {
+        let view = UIView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+//        view.clipsToBounds = false
+        view.backgroundColor = .red
+        
+        return view
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        view.backgroundColor = .brown
+        view.backgroundColor = .systemBackground
+        
+        setConstraints()
     }
 
-
+    
 }
 
+
+extension ViewController {
+    private func setConstraints() {
+        view.addSubview(squareView)
+        
+        let constant: CGFloat = 100
+        let sideOfSquare: CGFloat = 128
+        
+        let squareViewConstraints = [
+            squareView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            squareView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: constant),
+            squareView.widthAnchor.constraint(equalToConstant: sideOfSquare),
+            squareView.heightAnchor.constraint(equalToConstant: sideOfSquare)
+        ]
+        
+        NSLayoutConstraint.activate(squareViewConstraints)
+    }
+}
