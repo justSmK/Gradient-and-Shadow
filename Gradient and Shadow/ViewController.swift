cbead4fb80bb086bba2a769728b5d065d311368f
justSmK <28645140+justSmK@users.noreply.github.com>
2023-05-03 17:43:16 +0300

    The task was done

--- Gradient and Shadow/ViewController.swift
+++ Gradient and Shadow/ViewController.swift
@@ -9,13 +9,33 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    private let leadingAnchorConstant: CGFloat = 100
+    private let sideOfSquare: CGFloat = 128
+    private let cornerRadius: CGFloat = 13
+    
     private lazy var squareView: UIView = {
         let view = UIView()
         view.translatesAutoresizingMaskIntoConstraints = false
-//        view.clipsToBounds = false
         view.backgroundColor = .red
-        
+        view.layer.cornerRadius = cornerRadius
         return view
+    }()
+    
+    private lazy var gradientLayer: CAGradientLayer = {
+        let gradient = CAGradientLayer()
+        gradient.colors = [UIColor.red.cgColor,
+                           UIColor.green.cgColor,
+                           UIColor.orange.cgColor,
+                           UIColor.black.cgColor,
+                           UIColor.white.cgColor,
+                           UIColor.blue.cgColor,
+                           UIColor.gray.cgColor
+        ]
+        gradient.locations = [0, 0.1, 0.25, 0.4, 0.6, 0.8, 1]
+        gradient.startPoint = CGPoint(x: 0, y: 0)
+        gradient.endPoint = CGPoint(x: 1, y: 1)
+        gradient.cornerRadius = cornerRadius
+        return gradient
     }()
     
     override func viewDidLoad() {
@@ -25,21 +45,33 @@ class ViewController: UIViewController {
         
         setConstraints()
     }
-
     
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        gradientLayer.frame = squareView.bounds
+    }
+
 }
 
+extension ViewController {
+    private func setShadow() {
+        squareView.layer.shadowColor = UIColor.red.cgColor
+        squareView.layer.shadowOpacity = 1
+        squareView.layer.shadowOffset = CGSize(width: 5, height: 5)
+        squareView.layer.shadowRadius = 13
+    }
+}
 
+// MARK: Set Constraints
 extension ViewController {
     private func setConstraints() {
+        setShadow()
         view.addSubview(squareView)
-        
-        let constant: CGFloat = 100
-        let sideOfSquare: CGFloat = 128
+        squareView.layer.addSublayer(gradientLayer)
         
         let squareViewConstraints = [
             squareView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
-            squareView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: constant),
+            squareView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: leadingAnchorConstant),
             squareView.widthAnchor.constraint(equalToConstant: sideOfSquare),
             squareView.heightAnchor.constraint(equalToConstant: sideOfSquare)
         ]
@@ -47,3 +79,4 @@ extension ViewController {
         NSLayoutConstraint.activate(squareViewConstraints)
     }
 }
+
